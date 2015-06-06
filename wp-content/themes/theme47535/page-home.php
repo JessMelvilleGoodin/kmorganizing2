a0d62beba4c4ea7652d6bf2e2dabcf98021e59ec
Jessica Melville Goodin <jessmelvi@yahoo.com>
2015-06-05 23:33:09 -0500

    cropped home banner vertically

--- wp-content/themes/theme47535/page-home.php
+++ wp-content/themes/theme47535/page-home.php
@@ -7,7 +7,7 @@ get_header(); ?>
 
 <div class="motopress-wrapper content-holder clearfix">
 	<div data-motopress-type="static" data-motopress-static-file="static/static-slider.php">
-		<img src="http://www.kmorganizing.com/wp-content/uploads/2015/06/Header-banner01-for-web.jpg" alt="k m organizing banner">
+		<img id="home-banner" src="http://www.kmorganizing.com/wp-content/uploads/2015/06/Header-banner01-for-web-cropped.jpg" alt="k m organizing banner">
 	</div>
 	<div class="container">
 		<div class="row">
